99a3410862b3b7037b02ced61aee633f9bf68ed2
Harjinder Singh Sidhu <HarjinderS@axislighting.com>
2019-12-10 17:12:34 -0500

    Initial commit

---
+++ app/src/main/java/com/example/funfacts/FunFactsActivity.java
@@ -0,0 +1,14 @@
+package com.example.funfacts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FunFactsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fun_facts);
+    }
+}
